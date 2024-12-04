64311431711940a4864fb2bc780e2c62af494437
pokayfu <pokayyfu@gmail.com>
2024-12-04 19:56:34 +0300

    feat: add super user automatically in django service

--- movies_admin/scripts/entrypoint.sh
+++ movies_admin/scripts/entrypoint.sh
@@ -6,5 +6,5 @@ python manage.py collectstatic --no-input
 python manage.py waitfordb
 python manage.py migrate --fake movies 0001
 python manage.py migrate
-
+python manage.py createsuperuser --noinput || true
 uwsgi --ini uwsgi.ini
